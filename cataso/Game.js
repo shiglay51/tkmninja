c59952ecd89c892130e68ab4d725ed4ea4724d73
おまつ先生 <33661309+omatztw@users.noreply.github.com>
2021-05-04 16:07:06 +0900

    Fix

--- cataso/Game.js
+++ cataso/Game.js
@@ -435,7 +435,7 @@ Game.buildSettlement = function (game, index) {
 
 Game.isHataTriangle = function (game, index) {
     var active = game.active;
-    var tri = Const.HATA_TRI.map(ht => ht.filter(t => t === index));
+    var tri = Const.HATA_TRI.filter(ht => ht.some(t => t === index));
     return tri.some(t => t.every(i => (game.settlementList[i] & '0xF') ===  active));
 }
 
