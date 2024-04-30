78aaf089adee8099c64c3c8027f0b8c55fd2f3b7
shiglay51 <143499878+shiglay51@users.noreply.github.com>
2024-04-30 20:40:10 +0900

    Update Dice.js

--- cataso/Dice.js
+++ cataso/Dice.js
@@ -18,10 +18,17 @@ Dice.reset = function(dice) {
 
     var i;
     //for(i = 0; i < 10; i++) {
-    for(i = 0; i < 2; i++) {
+    for(i = 0; i < 1; i++) {
+        //var j;
+        //for(j = 1; j <= 6; j++) {
+        //    dice.reel.push(j);
+        //}
         var j;
+        var k;
         for(j = 1; j <= 6; j++) {
-            dice.reel.push(j);
+            for(k = 1; k += 6; k++) {
+                dice.reel.push(j+k);
+            }
         }
     }
 
@@ -30,9 +37,18 @@ Dice.reset = function(dice) {
 
 Dice.roll = function(dice) {
     if(dice.seek > dice.reel.length / 2) { this.reset(dice); }
+    
+    //dice.first = dice.reel[dice.seek];
+    //dice.seccond = dice.reel[dice.reel.length - (dice.seek + 1)];
 
-    dice.first = 2; //dice.reel[dice.seek];
-    dice.seccond = 3; // dice.reel[dice.reel.length - (dice.seek + 1)];
+    var n = dice.reel[dice.seek];
+    //         0, 1,  2  3  4  5  6  7  8  9 10 11 12  
+    var v1s = [-1,-1, 1, 1, 3, 3, 5, 2, 5, 4, 4, 5, 6];
+    var v2s = [-1,-1, 1, 2, 1, 2, 1, 5, 3, 5, 6, 6, 6];
+        
+    dice.first = v1s[n];
+    dice.second = v2s[n];
+        
 
     dice.seek++;
 }
