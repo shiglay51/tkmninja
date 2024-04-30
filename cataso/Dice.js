da2ec25e32eceb4263a16413c73451212a7e328c
shiglay51 <143499878+shiglay51@users.noreply.github.com>
2024-04-30 22:10:20 +0900

    Update Dice.js

--- cataso/Dice.js
+++ cataso/Dice.js
@@ -19,7 +19,7 @@ Dice.reset = function(dice) {
     var i;
     for(i = 0; i < 10; i++) {
         var j;
-        for(j = 1; j <= 6; j++) {
+        for(j = 2; j <= 6; j++) {
             dice.reel.push(j);
         }
     }
@@ -36,6 +36,16 @@ Dice.roll = function(dice) {
     var v1s = [1];
     dice.first = v1s[0];
     
+    var n = dice.reel[dice.seek];
+    //         0,  1, 2  3  4  5  6  7  8  9 10 11 12  
+    var v1s = [-1,-1, 1, 1, 3, 3, 5, 2, 5, 4, 4, 5, 6];
+    var v2s = [-1,-1, 1, 2, 1, 2, 1, 5, 3, 5, 6, 6, 6];
+    var v1s = new Array(-1,-1, 1, 1, 3, 3, 5, 2, 5, 4, 4, 5, 6);
+    var v2s = new Array(-1,-1, 1, 2, 1, 2, 1, 5, 3, 5, 6, 6, 6);
+    
+    //dice.first = v1s[n];
+    //dice.second = v2s[n];
+        
     dice.seek++;
 }
 
